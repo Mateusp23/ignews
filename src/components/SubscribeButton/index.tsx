3102582e4e77c030ad9bbe2fad8e3aabc767e02b
Mateus Paulart <mateusp23@rede.ulbra.br>
2022-06-20 16:36:54 -0300

    gerando sessão do checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,27 @@
 import styles from './styles.module.scss';
+import { useSession, signIn } from 'next-auth/react';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  const handleSubscribe = () => {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    
+  }
+
   return ( 
     <button 
       className={styles.subscribeButton}
       type="button"
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
